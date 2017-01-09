90850f95fdda2d6ace2ea5614aa08931211c6132
Lazar-Mihai <mihai2812@yahoo.com>
2017-01-09 23:31:09 +0200

    header commit tabla
    functii pentru tabla de joc

---
+++ chessHeader.h
@@ -0,0 +1,35 @@
+
+
+
+
+
+void afisareCasute();
+
+
+void select(int x, int y);
+
+
+void afisare();
+
+
+void neg(int x, int y);
+
+
+void alb(int x, int y);
+
+
+void casutaAlbao(int x, int y);
+
+
+void casutaAlban(int xx, int yy);
+
+
+void casutaAlbaoo(int x, int y);
+
+
+void casutaAlbann(int xx, int yy);
+
+
+
+
+
